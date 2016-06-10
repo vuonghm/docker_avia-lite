d96b75fce908da83f0ec303757d0f44c0b060916
root <root@fr-s-bsg-avia-t.ncifcrf.gov>
2016-06-10 13:17:02 -0400

    Changed the destination of retrieve_a_request

--- src/retrieve_a_request.php
+++ src/retrieve_a_request.php
@@ -4,12 +4,12 @@ include_once("xml/navigation.php");
 include_once("xml/side_nav.php");
 ?>
 <div id="column-two">
-<form method="post" name="myform" action="successful_retrieval.php">
+<form method="post" name="myform" action="results.php">
 <h2> Retrieve a request that was previously submitted to AVIA </h2>
 <table id="formatted"><tr><td colspan="3"><font color="#993300"> A field with an asterisk (*) before it is a required field. </font></td></tr>
 <tr>
 	<td><font color="#993300">*Request Id</font></td>
-	<td><input type="text" name="user_requestor_id" placeholder="Enter your AVIA-lite id" size="40"/></td>
+	<td><input type="text" name="id" placeholder="Enter your AVIA-lite id" size="40"/></td>
 </tr>
 </table>
 <br />
